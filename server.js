42b6f38e6cc16c5db812bf3fb08c4fdeb7db76dd
Anton Pakhomov <antonn.pakhomovv@gmail.com>
2025-04-20 20:45:30 +0400

    add faker

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const { koaBody } = require('koa-body');
 const cors = require('@koa/cors');
-const uuid = require('uuid');
+const { faker } = require('@faker-js/faker');
 
 const app = new Koa();
 const router = new Router();
@@ -13,29 +13,35 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
 
-const fakeRes = {
-	status: 'ok',
-	timestamp: 1553400000,
-	messages: [
-		{
-			id: uuid.v4(),
-			from: 'anya@ivanova',
-			subject: 'Hello from Anya',
-			body: 'Long message body here',
-			received: 1553108200,
-		},
-		{
-			id: uuid.v4(),
-			from: 'alex@petrov',
-			subject: 'Hello from Alex Petrov!',
-			body: 'Long message body here',
-			received: 1553107200,
-		},
-	],
-};
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 router.get('/messages/unread', async ctx => {
-	ctx.response.body = fakeRes;
+	if (Math.random() > 0.9) {
+		ctx.response.status = 500;
+		ctx.response.body = { status: 'nothing' };
+	} else {
+		const fakeRes = {
+			status: 'ok',
+			timestamp: Date.now(),
+			messages: [],
+		};
+
+		const countMessagesToSend = getRandomInt(0, 10);
+
+		for (let i = 0; i < countMessagesToSend; i++) {
+			fakeRes.messages.push({
+				id: faker.string.uuid(),
+				from: faker.internet.email(),
+				subject: faker.word.words({ count: { min: 2, max: 5 } }),
+				body: faker.word.words({ count: { min: 5, max: 10 } }),
+				received: faker.date.between({ from: '2000-01-01', to: Date.now() }),
+			});
+		}
+
+		ctx.response.body = fakeRes;
+	}
 });
 
 http.createServer(app.callback()).listen(port);
